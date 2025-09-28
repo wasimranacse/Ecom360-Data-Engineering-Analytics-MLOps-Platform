720b51c9cbd6bbe90fd3e2b37e02feac48baaf08
wasimranacse <wrana697@gmail.com>
2025-09-28 14:43:34 +0100

    Project Architecture and Mapping DONE

---
+++ src/ingest.py
@@ -0,0 +1,6 @@
+# │── src/
+# │   ├── ingest.py             # Data ingestion from WooCommerce
+# │   ├── transform.py          
+# │   ├── train.py              
+# │   ├── drift.py              
+# │   ├── app.py               
